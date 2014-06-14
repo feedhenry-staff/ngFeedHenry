926c44f13a2c3d420ad13e26c9be858a741992bc
Evan Shortiss <evan.shortiss@feedhenry.com>
2014-06-14 15:10:47 +0100

    Declare deps in services

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = angular.module('ngFH', [])
-  .service('Utils', require('./Utils.js'))
-  .service('Log', require('./Log.js'))
-  .service('Act', ['Utils, Log, $q, $window, $timeout', require('./Act.js')]);
+  .service('Utils', ['$rootScope', require('./Utils.js')])
+  .service('Log',   ['$window', require('./Log.js')])
+  .service('Act',   ['Utils, Log, $q, $window, $timeout', require('./Act.js')]);
