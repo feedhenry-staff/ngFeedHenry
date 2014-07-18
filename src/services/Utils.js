b14229d074b590c182caa391036207558d2e8f04
Evan Shortiss <evan.shortiss@feedhenry.com>
2014-07-18 12:57:19 +0100

    Add utils for generic tasks

--- src/services/Utils.js
+++ src/services/Utils.js
@@ -6,7 +6,7 @@ module.exports = function ($rootScope) {
    * Safely call a function that modifies variables on a scope.
    * @param {Function} fn
    */
-  this.safeApply = function (fn) {
+  var safeApply = this.safeApply = function (fn) {
     var phase = $rootScope.$$phase;
 
     if (phase === '$apply' || phase === '$digest') {
@@ -18,5 +18,38 @@ module.exports = function ($rootScope) {
     }
   };
 
+
+  /**
+   * Check for an internet connection
+   */
+  this.isOnline = function () {
+    return $window.navigator.onLine;
+  };
+
+
+  /**
+   * Wrap a success callback in Node.js style.
+   * @param {Function}
+   */
+  this.onSuccess = function (fn) {
+    return function (res) {
+      safeApply(function () {
+        fn (null, res);
+      });
+    };
+  };
+
+
+  /**
+   * Wrap a fail callback in Node.js style.
+   * @param {Function}
+   */
+  this.onFail = function (fn) {
+    return function (err) {
+      safeApply(function () {
+        fn (err, null);
+      });
+    };
+  };
 
 };
