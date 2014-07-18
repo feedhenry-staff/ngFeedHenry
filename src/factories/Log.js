611851e272e0d6e052239afb613f631d629a816a
Evan Shortiss <evan.shortiss@feedhenry.com>
2014-07-18 12:54:07 +0100

    Add log factory

---
+++ src/factories/Log.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var hlog = require('hlog')
+
+module.exports = function () {
+  // Simply use hlog! May change...
+  return hlog;
+};
---
+++ src/factories/index.js
@@ -0,0 +1,4 @@
+'use strict';
+
+module.exports = angular.module('ngFH')
+  .service('Log', require('./Log.js'));
