3e680711cc56cb683962f134d6bfe0ce9c925b99
Evan Shortiss <evan.shortiss@feedhenry.com>
2014-09-09 17:54:50 -0400

    Add in mocks and use mcoha

--- karma.conf.js
+++ karma.conf.js
@@ -4,20 +4,20 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'chai'],
+    frameworks: ['mocha', 'chai'],
 
 
     // list of files / patterns to load in the browser
     // we need angular and it's mocks for testing/loading modules
     files: [
       './bower_components/angular/angular.js',
-      './bower_components/angular/angular-mocks.js',
-      './bower_components/fh-js-sdk/dist/feedhenry.js',
+      './bower_components/angular-mocks/angular-mocks.js',
+      './node_modules/fh-mocks/dist/fh-mocks.js',
       './test/bundle.js',
-      './test`/**/*.js'
+      './test/**/*.js'
     ],
 
-    reporters: ['progress'],
+    reporters: ['mocha'],
     port: 9876,
     colors: true,
     logLevel: 'DEBUG',
